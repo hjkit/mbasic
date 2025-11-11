0b0ff6fe120a1d4193817d487647452c54f8bb60
Aaron Wohl <mbasic@wohl.com>
2025-11-11 14:13:35 -0500

    Clean up project root - remove compiler test artifacts and generated files

--- size_test_fwrite.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-    char *s = "Hello World";
-    fwrite(s, 1, 11, stdout);
-    fputc('\n', stdout);
-    return 0;
-}
--- size_test_printf.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-    char *s = "Hello World";
-    printf("%.*s\n", 11, s);
-    return 0;
-}
